f41eb3a704523d628562ecfc3115c8784869661f
HKM <rbals0445@naver.com>
2021-08-28 23:01:08 +0900

    Node callbacks and error handling

--- NodeJS/node_examples/index.js
+++ NodeJS/node_examples/index.js
@@ -1,16 +1,23 @@
 var rect = require("./rectangle");
 //import { perimeter } from "./rectangle";
 
-function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b);
-    if( l <= 0 || b <= 0){
-        console.log("Rectangle Dimensions should be greater than zero : l = "+ l + ", and b = " + b);
-    }else{
-        console.log("The area of the rectangle is "+ rect.area(l,b));
-        console.log("The perimeter of the rectagle is "+ rect.perimeter(l,b));
+function solveRect(l, b) {
+  console.log("Solving for rectangle with l = " + l + " and b = " + b);
+  rect(l, b, (err, rectangle) => {
+    if (err) {
+      console.log(`ERROR : ${err.message}`);
+    } else {
+      console.log(
+        `The area of rectangle of dimensions l = ${l} and b = ${b} is  ${rectangle.area()}`
+      );
+      console.log(
+        `The perimeter of rectangle of dimensions l = ${l} and b = ${b} is  ${rectangle.perimeter()}`
+      );
     }
-    //console.log("tjis "+ perimeter(l,b));
+  });
+  console.log("This statement is after the call to rect()");
+  //console.log("tjis "+ perimeter(l,b));
 }
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,-4);
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, -4);
--- NodeJS/node_examples/rectangle.js
+++ NodeJS/node_examples/rectangle.js
@@ -1,2 +1,26 @@
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area = (x,y) => (x*y);
+module.exports = (x, y, callback) => {
+  if (x <= 0 || y <= 0) {
+    setTimeout(
+      () =>
+        callback(
+          new Error(
+            "Rectangle Dimensions should be greater than zero : l = " +
+              x +
+              ", and b = " +
+              y
+          ),
+          null
+        ),
+      2000
+    );
+  } else {
+    setTimeout(
+      () =>
+        callback(null, {
+          perimeter: () => 2 * (x + y),
+          area: () => x * y, // parameter로 들어옴
+        }),
+      2000
+    );
+  }
+};
